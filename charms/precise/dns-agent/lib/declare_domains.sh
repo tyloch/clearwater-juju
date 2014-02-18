b368f61c0a1ffb5f9f180f65d244ebf6775439c2
Andy Caldwell <andrew.caldwell@metaswitch.com>
2014-02-18 12:54:47 +0000

    Add ports to domain names

--- charms/precise/dns-agent/lib/declare_domains.sh
+++ charms/precise/dns-agent/lib/declare_domains.sh
@@ -9,8 +9,9 @@ fi
 
 relation-set home_domain=$root_zone
 relation-set sprout_domain=sprout.$root_zone
-relation-set homestead_domain=homestead.$root_zone
-relation-set homer_domain=homer.$root_zone
+relation-set homestead_domain=homestead.$root_zone:8888
+relation-set provisioning_domain=homestead.$root_zone:8889
+relation-set homer_domain=homer.$root_zone:9888
 relation-set ellis_domain=ellis.$root_zone
 relation-set ralf_domain=ralf.$root_zone
 relation-set sas_domain=`config-get sas`
